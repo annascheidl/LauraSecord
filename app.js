079339d268dab4e061ee3f6f60f6118a9fdfd6ff
annascheidl <60186133+annascheidl@users.noreply.github.com>
2021-04-07 13:01:43 -0400

    End destination update

--- app.js
+++ app.js
@@ -1,16 +1,80 @@
 const express   = require('express');
 const app       = express();
 const http      = require('http');
-const server    = require('http').createServer(app);  
+const server    = http.createServer(app);  
+const io        = require('socket.io')(server);
 
-const LISTEN_PORT = 8080;
+const LISTEN_PORT   = 8080;
 
 server.listen(LISTEN_PORT);
 app.use(express.static(__dirname + '/public')); //set root path of server ...
-
 console.log("Listening on port: " + LISTEN_PORT );
 
-//this is call a "route" - basically a url path from your website for static pages
+//our routes
 app.get( '/', function( req, res ){ 
     res.sendFile( __dirname + '/public/index.html' );
+});
+
+app.get( '/lauraSecordPlayer', function( req, res ){ 
+    res.sendFile( __dirname + '/public/lauraSecordPlayer.html' );
+});
+
+app.get( '/soldierPlayer', function( req, res ){ 
+    res.sendFile( __dirname + '/public/soldierPlayer.html' );
+});
+
+//websockets
+//required socket.io server pieces from networked-aframe server example: 
+const rooms = {};
+
+io.on("connection", socket => {
+  console.log("user connected", socket.id);
+
+  let curRoom = null;
+
+  socket.on("joinRoom", data => {
+    const { room } = data;
+
+    if (!rooms[room]) {
+      rooms[room] = {
+        name: room,
+        occupants: {},
+      };
+    }
+
+    const joinedTime = Date.now();
+    rooms[room].occupants[socket.id] = joinedTime;
+    curRoom = room;
+
+    console.log(`${socket.id} joined room ${room}`);
+    socket.join(room);
+
+    socket.emit("connectSuccess", { joinedTime });
+    const occupants = rooms[room].occupants;
+    io.in(curRoom).emit("occupantsChanged", { occupants });
+  });
+
+  socket.on("send", data => {
+    io.to(data.to).emit("send", data);
+  });
+
+  socket.on("broadcast", data => {
+    socket.to(curRoom).emit("broadcast", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log('disconnected: ', socket.id, curRoom);
+    if (rooms[curRoom]) {
+      console.log("user disconnected", socket.id);
+
+      delete rooms[curRoom].occupants[socket.id];
+      const occupants = rooms[curRoom].occupants;
+      socket.to(curRoom).emit("occupantsChanged", { occupants });
+
+      if (occupants == {}) {
+        console.log("everybody left room");
+        delete rooms[curRoom];
+      }
+    }
+  });
 });
